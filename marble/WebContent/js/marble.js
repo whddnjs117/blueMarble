77d666cf01eb26ca8152ae28baa5527f913f888c
GD3 <GD3@DESKTOP-NF6773I>
2019-11-04 18:12:26 +0900

    무인도 3회시 탈출하게하고 더블이나오면 주사위한번더 굴려서 탈출할수있게설정완료 참여한 플레이어의 게임머니나 보유한 땅을 출력할 테이블준비

--- marble/WebContent/js/marble.js
+++ marble/WebContent/js/marble.js
@@ -13,7 +13,8 @@ let player1 = {
 		playerImg : "player1.png", //이미지이름
 		player : "player1", //html 클래스값
 		money : 3000000,
-		playTurn : 0 // 더블을 3턴하면 무인도로이동
+		playTurn : 0, // 더블을 3턴하면 무인도로이동
+		nowPoint : 21
 }
 let player2 = {
 		beforePoint : 21,
@@ -23,7 +24,8 @@ let player2 = {
 		playerImg : "player2.png",
 		player : "player2",
 		money : 3000000,
-		playTurn : 0
+		playTurn : 0,
+		nowPoint : 21
 }
 
 let player3 = {
@@ -34,7 +36,8 @@ let player3 = {
 		playerImg : "player3.png",
 		player : "player3",
 		money : 3000000,
-		playTurn : 0
+		playTurn : 0,
+		nowPoint : 21
 }
 
 let player4 = {
@@ -45,7 +48,8 @@ let player4 = {
 		playerImg : "player4.png",
 		player : "player4",
 		money : 3000000,
-		playTurn : 0
+		playTurn : 0,
+		nowPoint : 21
 }
 let playCount = 1; //플레이어의 순서값
 let playTurn = 0; //무인도에 갇혀있을때 턴횟수를제한
@@ -53,46 +57,46 @@ let player = {}; //버튼눌럿을때 나오는 플레이어를 저장할 객체
 let playerMembers = 0; //사람수를 선택할수있는 플레이어
 //------------------------------------------------------------------------------맵 데이터
 let MapData = [
-	null,
+	{type : null , title : null},
 	{type : "money" , title : "사회복지"},
-	{type : "city" , title : "부에노스"},
+	{type : "city" , title : "부에노스", owner : null},
 	{type : "chance", title : "황금열쇠"},
-	{type : "city" , title : "상파울루"},
-	{type : "city" , title : "시드니"},
-	{type : "island" , title : "부산"},
-	{type : "city" , title : "하와이"},
-	{type : "city" , title : "리스본"},
-	{type : "island" , title : "엘리자베스"},
-	{type : "city" , title : "마드리드"},
+	{type : "city" , title : "상파울루", owner : null},
+	{type : "city" , title : "시드니", owner : null},
+	{type : "island" , title : "부산", owner : null},
+	{type : "city" , title : "하와이", owner : null},
+	{type : "city" , title : "리스본", owner : null},
+	{type : "island" , title : "엘리자베스", owner : null},
+	{type : "city" , title : "마드리드", owner : null},
 	{type : "space" , title : "우주여행"},
-	{type : "city" , title : "도쿄"},
-	{type : "island" , title : "컬럼비아호"},
-	{type : "city" , title : "파리"},
-	{type : "city" , title : "로마"},
+	{type : "city" , title : "도쿄", owner : null},
+	{type : "island" , title : "컬럼비아호", owner : null},
+	{type : "city" , title : "파리", owner : null},
+	{type : "city" , title : "로마", owner : null},
 	{type : "chance" , title : "황금열쇠"},
-	{type : "city" , title : "런던"},
-	{type : "city" , title : "뉴욕"},
+	{type : "city" , title : "런던", owner : null},
+	{type : "city" , title : "뉴욕", owner : null},
 	{type : "pay" , title : "기부"},
-	{type : "island" , title : "서울"},
+	{type : "island" , title : "서울", owner : null},
 	{type : "home" , title : "출발지"},
-	{type : "city" , title : "타이베이"},
+	{type : "city" , title : "타이베이", owner : null},
 	{type : "chance" , title : "황금열쇠"},
-	{type : "city" , title : "베이징"},
-	{type : "city" , title : "마닐라"},
-	{type : "island" , title : "제주"},
-	{type : "city" , title : "싱가포르"},
+	{type : "city" , title : "베이징", owner : null},
+	{type : "city" , title : "마닐라", owner : null},
+	{type : "island" , title : "제주", owner : null},
+	{type : "city" , title : "싱가포르", owner : null},
 	{type : "chance" , title : "황금열쇠"},
-	{type : "city" , title : "카이로"},
-	{type : "city" , title : "이스탄불"},
+	{type : "city" , title : "카이로", owner : null},
+	{type : "city" , title : "이스탄불", owner : null},
 	{type : "desert_island" , title : "무인도"},
-	{type : "city" , title : "아테네"},
+	{type : "city" , title : "아테네", owner : null},
 	{type : "chance" , title : "황금열쇠"},
-	{type : "city" , title : "코펜하겐"},
-	{type : "city" , title : "스톡홀름"},
+	{type : "city" , title : "코펜하겐", owner : null},
+	{type : "city" , title : "스톡홀름", owner : null},
 	{type : "island" , title : "여객기"},
-	{type : "city" , title : "베른"},
-	{type : "city" , title : "베를린"},
-	{type : "city" , title : "오타와"}
+	{type : "city" , title : "베른", owner : null},
+	{type : "city" , title : "베를린", owner : null},
+	{type : "city" , title : "오타와", owner : null}
 ];
 //--------------------------------------------------------------맵데이터 끝
 window.addEventListener("load",function() {
@@ -115,11 +119,11 @@ window.addEventListener("load",function() {
 		}else{
 			playCount=1;
 		}
-		let dice3 = Math.floor((Math.random() * 6) + 1);
-		let dice1 = 1;
+		let dice3 = Math.floor((Math.random() * 3) + 1);
+		let dice1 = 4;
 		$("#onedice").attr("src","/marble/dice/dice"+dice1+".jpg")
-		let dice4 = Math.floor((Math.random() * 6) + 1);
-		let dice2 = 1;
+		let dice4 = Math.floor((Math.random() * 3) + 1);
+		let dice2 = 6;
 		$("#twodice").attr("src","/marble/dice/dice"+dice2+".jpg")
 		$("#onedice").show();
 		$("#twodice").show();
@@ -135,17 +139,19 @@ window.addEventListener("load",function() {
 		// 말을 이동시킵니다.
 		console.log("플레어이 after 포인트"+player.afterPoint)
 		console.log("플레어이 before 포인트"+player.beforePoint)
-		if(MapData[player.beforePoint].title == "무인도"){
+		if(MapData[player.nowPoint].title == "무인도"){
 			console.log("현재 섬은 무인도이다.")
-			if(dice1 == dice2){
-				playCount -= 1;
-				player.playTurn = 3;
-			} else {
-				console.log("무인도 탈출실패!!");
-				Desert_isalnd();
+			if(DesertIsalnd(dice1,dice2)){
+				return;
+			}else if(player.playTurn==2){
+				alert("다음턴에 무인도를 나가실수 있습니다.");
+				player.nowPoint = 0;
+				return;
+			}else{
+				player.playTurn += 1;
+				alert("무인도탈출 실패 현재턴 :"+player.playTurn);
 				return;
 			}
-			
 		}
 		if(dice1==dice2){ // 더블이발생할경우 함수를 호출
 			Double();
@@ -154,7 +160,15 @@ window.addEventListener("load",function() {
 			player.playTurn = 0;
 		}
 		MovePlayer();
-//		player.afterId = "#p" + player.afterPoint;
+		
+		if(MapData[player.afterPoint].type == "city"){ //말을 이동시키고 나서 도착한 위치가 도시일경우
+			console.log("도시도착함")
+			MapData[player.afterPoint].owner = player.player;
+			console.log(MapData[player.afterPoint]);
+		}
+		
+		
+//		player.afterId = "#p" + player.afterPoint; 함수화
 //		player.beforeId = "#p" + player.beforePoint;
 //		console.log("현재 이동하는말은 :"+player.player);
 //		console.log("이동한 칸수는 : "+ran);
@@ -188,25 +202,27 @@ window.addEventListener("load",function() {
 		if(player.playTurn==3){
 			alert("더블을 3번하셧으므로 무인도로 이동합니다.");
 			player.afterPoint = 31;
-			console.log("더블이 3번 발생해서 무인도로갈 값을 저장")
+			console.log("더블이 3번 발생해서 무인도로갈 값을 저장하고 무인도로 이동")
 			playCount +=1;
 			player.playTurn = 0;
 		}
 	};
-	function Desert_isalnd() {
-		playCount +=1;
-		player.playTurn +=1;
-		if(player.playTurn==3){
-			playCount-=1;
-			player.playTurn = 0;
+	function DesertIsalnd(a,b) { //현재위치하고있는 장소가 무인도일 경우 발생하는 함수
+		if(a==b){ //주사위가 같은값을 가질경우
+			alert("주사위 값이 같아 무인도를 탈출하셧습니다.");
+			playCount -= 1; //더블나와서 탈출할경우 플레이어가 한번더 돌리기위해
+			player.playTurn = 0; // 플레이의 턴을 초기화시켜준다.
+			player.nowPoint = 0; // 플레이의 현재위치포인트값을 초기화
+			return true;
 		}
 	}
-	function MovePlayer() {
+	function MovePlayer() { //말이동하는 함수
 		player.afterId = "#p" + player.afterPoint;
 		player.beforeId = "#p" + player.beforePoint;
 		console.log("현재 이동하는말은 :"+player.player);
 		$("."+player.player).remove();
 		$(player.afterId).append("<input type='image' src='/marble/image/"+player.playerImg+"' class='player "+player.player+"'>");
 		player.beforePoint = player.afterPoint;
+		player.nowPoint = player.beforePoint;
 	}
 });
